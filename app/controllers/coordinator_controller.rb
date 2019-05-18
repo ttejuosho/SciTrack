1e8f593a32d72374a0bde4088f08d0086d281240
ttejuosho <theycallmeflowz@yahoo.com>
2019-05-18 04:56:41 -0500

    coord enhancements

--- app/controllers/coordinator_controller.rb
+++ app/controllers/coordinator_controller.rb
@@ -1,6 +1,6 @@
 class CoordinatorController < ApplicationController
-    #before_action :coordinator, only: [:show, :edit, :update, :destroy]
-    #get all coordinators
+#before_action :coordinator, only: [:show, :edit, :update, :destroy]
+#get all coordinators
     def index
         @coordinators = Coordinator.all
         respond_to do |format|
@@ -8,18 +8,22 @@ class CoordinatorController < ApplicationController
         end
     end
 
-    # def show
-    #     @coordinators = Coordinator.all
-    #     render json: @coordinators
-    # end
-
-#get coordinators for a registry
+#get One coordinator
     def show
         @coordinator = Coordinator.find(params[:id])
         respond_to do |format|
             format.html { render :coordinator_view  }
         end
     end
+    
+#get all coordinators for a coordinator
+    def show_coordinators
+        @coordinator = Coordinator.find(params[:id])
+        respond_to do |format|
+            format.html { render :coordinator  }
+        end
+    end
+
 #Change Coordinator information
     def edit
         @coordinator = Coordinator.find(params[:id])
@@ -27,13 +31,15 @@ class CoordinatorController < ApplicationController
             format.html { render :coordinator_edit  }
           end
     end
-    #New coordinator form
+
+#New coordinator form
     def new
         @coordinator = Coordinator.new
         respond_to do |format|
             format.html { render :coordinator_new  }
-          end
+        end
     end
+
 #Save new Coordinator to DB
     def create
         @coordinator = Coordinator.new(coordinator_params)
@@ -48,8 +54,29 @@ class CoordinatorController < ApplicationController
           end
     end
 
+#Update Coordinator information
+    def update
+        @coordinator = Coordinator.find(params[:id])
+        if @coordinator.update(coordinator_params)
+            redirect_to coordinator_path
+        else
+            render :coordinator_edit
+        end
+    end
+
+#delete Coordinator
+    def destroy
+        @coordinator = Coordinator.find(params[:id])
+        @coordinator.destroy
+        redirect_to coordinator_index_path
+    end
+
+    def coordinator_param
+        params.require(:coordinator).permit(:name, :email, :phone_number, :coordinator_id)
+    end
+
     private
     def coordinator_params
-      params.permit(:name, :email, :phone_number)
+        params.permit(:name, :email, :phone_number, :coordinator_id)
     end
 end
